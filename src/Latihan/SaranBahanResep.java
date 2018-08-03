438ba56117c4130d079b5f28ced6a1eda3d30a39
GL62 <GL62@MSI>
2018-08-02 20:16:22 -0800

    INPUT BAHAN 1

---
+++ src/Latihan/SaranBahanResep.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+import java.util.Scanner;
+public class SaranBahanResep {
+    public static void main(String[] args) {
+        String identitas="Ahmad Dwiyan Anugrah Putra / RPL 5 /3";
+        System.out.println("Identitas"+identitas);
+        
+        System.out.println("\nSaran Resep dari Bahan Milik Anda\n");
+        
+        Scanner masukan = new Scanner(System.in);
+        
+        System.out.println("BAHAN PERTAMA: ");
+         System.out.println("Bahan 1: Pisang");
+         System.out.println("BAHAN 2 :Telur");
+         System.out.print("MASUKAN PILIHANMU");
+         int bahan1 = masukan.nextInt();
+         
+         
+        
+        
+        
+}
+}
